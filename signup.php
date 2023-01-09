93ef783f224fe40a6b74b6e3e6a2bc418b94e9c6
Daweendri <83944867+Daweendri@users.noreply.github.com>
2023-01-09 09:22:19 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
     <title>Signup</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
-<body>
+<body style="background-image: url('icon/common.jpg');background-repeat:no-repeat;background-attachment:fixed;background-size:cover;">
     <center>
         <div class="login-box">
             <form method="post" action="php/signup.php" enctype="multipart/form-data">
